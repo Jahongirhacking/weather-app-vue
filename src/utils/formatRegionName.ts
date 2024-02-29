4e03aaa98f48e570c79ad9b8398b318d17552818
Jahongir Hayitov <66916141+Jahongirhacking@users.noreply.github.com>
2024-02-29 08:21:07 +0500

    fix: change condition
    qoraqalpog'iston respublikasi

--- src/utils/formatRegionName.ts
+++ src/utils/formatRegionName.ts
@@ -2,7 +2,7 @@ const formatRegionName = (region: { name: string }) => {
   return `${region.name.charAt(0).toUpperCase()}${region.name.slice(1)} ${
     region.name === "toshkent"
       ? "shahri"
-      : region.name === "qoraqalpogiston"
+      : region.name === "qoraqalpog'iston"
       ? "Respublikasi"
       : "viloyati"
   }`;
